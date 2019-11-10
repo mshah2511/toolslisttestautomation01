353b227b5c1276935e161ab93cbe780e840cea56
Mishal Shah <mishal.shah@somoglobal.com>
2019-11-10 18:59:18 +0000

    adding more steps to driver and stepdefs

--- src/test/java/co/uk/cinch/Stepdefs.java
+++ src/test/java/co/uk/cinch/Stepdefs.java
@@ -1,12 +1,37 @@
 package co.uk.cinch;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import utils.Driver;
 
 import static org.junit.Assert.*;
 
 public class Stepdefs {
+
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+        // Get the driver for the browser (e.g. ChromeDriver)
+        driver = Driver.getDriver();
+        // Navigate to the URL of our webpage
+        driver.get(Driver.getUrl());
+    }
+
+    @After
+    public void tearDown(){
+
+        try {
+            Thread.sleep(Integer.parseInt(System.getProperty("sleep", "2000")));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.quit();
+    }
 
     @Given("I am logged out")
     public void i_am_logged_out() {
--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -4,10 +4,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver {
 
-    private static final String URL = System.getProperty("url", "www-test.cinch.somo-tools.com/");
+    private static final String URL = System.getProperty("url", "https://www-test.cinch.somo-tools.com/");
     private static final String BROWSER = System.getProperty("browser", "chrome");
 
     public static String getUrl() {
@@ -31,6 +32,14 @@ public class Driver {
 
                 // Return our Driver
                 return new ChromeDriver();
+
+            case "FIREFOX":
+
+                System.out.println("Executing on FIREFOX");
+
+                WebDriverManager.firefoxdriver().setup();
+
+                return new FirefoxDriver();
 
             default:
                 throw new IllegalArgumentException("The Browser Type is Undefined");
