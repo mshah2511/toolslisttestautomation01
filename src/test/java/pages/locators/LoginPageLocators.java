40644fd03c1b90289267f3a95b14c18f7acdaf53
Mishal Shah <mishal.shah@somoglobal.com>
2020-02-05 12:02:13 +0000

    general update and save

--- src/test/java/pages/locators/LoginPageLocators.java
+++ src/test/java/pages/locators/LoginPageLocators.java
@@ -1,17 +1,13 @@
 package pages.locators;
 
-import lombok.Data;
 import org.openqa.selenium.By;
 
 public class LoginPageLocators {
 
-    //fields
     private By usernameLocator = By.id("Username");
     private By passwordLocator = By.id("Password");
+    private By loginButtonLocator = By.className("ase-button-module_base primary-button-module_button base-button-module_regular base-button-module_full-width login_button");
 
-    //buttons
-    private By loginButtonLocator = By.name("button");
-
-    //messages
     private By failedLoginMessage = By.className("validation-summary-errors");
+
 }
